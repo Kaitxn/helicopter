736c6f7834ccbb3b65d646515e9c3cfc90c829c8
Yuooshi <yuookii@proton.me>
2023-08-21 23:50:54 +0800

    for kindergarderners

---
+++ calculator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+        double num1;
+    double num2;
+        printf(" __________\n");
+    printf("| ________ |\n");
+    printf("||12345678||\n");
+    printf("|\"\"\"\"\"\"\"\"\"\"|\n");
+    printf("|[M|#|C][-]|\n");
+    printf("|[7|8|9][+]|\n");
+    printf("|[4|5|6][x]|\n");
+    printf("|[1|2|3][%%]|\n");
+    printf("|[.|O|:][=]|\n");
+    printf("\"----------\"\n");
+    printf("first number: ");
+    scanf ("%lf", &num1);
+    printf("second number: ");
+    scanf ("%lf", &num2);
+
+printf("Answer: %f", num1 + num2);
+printf(" la dumbass");
+}
--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("pussy");
-    return 0; 
-}
-
